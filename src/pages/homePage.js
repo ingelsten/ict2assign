42fa94b2d13eb425968e6516d42130523efa23c1
Anders <77015471+ingelsten@users.noreply.github.com>
2022-06-13 21:19:32 +0100

    Added filtering of movie list.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,22 +1,49 @@
-import React, { useState, useEffect } from "react";  // Changed
+import React, { useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
+import FilterCard from "../components/filterMoviesCard";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import MovieList from "../components/movieList";
+import Fab from "@material-ui/core/Fab";
+import Drawer from "@material-ui/core/Drawer";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) =>  ({
   root: {
     padding: "20px",
   },
-});
+  fab: {
+    marginTop: theme.spacing(8),
+    position: "fixed",
+    top: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
 
 const MovieListPage = (props) => {
   const classes = useStyles();
   const [movies, setMovies] = useState([]);
+  const [titleFilter, setTitleFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const genreId = Number(genreFilter);
+
+  let displayedMovies = movies
+    .filter((m) => {
+      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
+    })
+    .filter((m) => {
+      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+    });
+
+  const handleChange = (type, value) => {
+    if (type === "title") setTitleFilter(value);
+    else setGenreFilter(value);
+  };
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=true&page=1`
     )
       .then((res) => res.json())
       .then((json) => {
@@ -30,14 +57,35 @@ const MovieListPage = (props) => {
   }, []);
 
   return (
+    <>
     <Grid container className={classes.root}>
       <Grid item xs={12}>
         <Header title={"Home Page"} />
       </Grid>
       <Grid item container spacing={5}>
-        <MovieList movies={movies}></MovieList>
+        <MovieList movies={displayedMovies}></MovieList>
       </Grid>
     </Grid>
+    <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() => setDrawerOpen(true)}
+        className={classes.fab}
+      >
+        Filter
+      </Fab>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <FilterCard
+          onUserInput={handleChange}
+          titleFilter={titleFilter}
+          genreFilter={genreFilter}
+        />
+      </Drawer>
+    </>
   );
 };
 export default MovieListPage;
