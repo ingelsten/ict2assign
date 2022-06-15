9b28c69b07b45f4af0b0719c7aead8d9167d2edc
Anders <77015471+ingelsten@users.noreply.github.com>
2022-06-15 20:37:16 +0100

    Change site header.

--- src/components/templateMoviePage/index.js
+++ src/components/templateMoviePage/index.js
@@ -33,8 +33,9 @@ const TemplateMoviePage = ({ movie, children }) => {
   }, []);
 
   return (
-    <>
-      <MovieHeader movie={movie} />
+    
+    <div className={classes.root}>
+    <MovieHeader movie={movie} />
 
       <Grid container spacing={5} style={{ padding: "15px" }}>
         <Grid item xs={3}>
@@ -56,7 +57,7 @@ const TemplateMoviePage = ({ movie, children }) => {
           {children}
         </Grid>
       </Grid>
-    </>
+    </div>
   );
 };
 
