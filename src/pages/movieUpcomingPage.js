ce472d4766dba67cbc189ab149b33ef97982d536
Anders <77015471+ingelsten@users.noreply.github.com>
2022-07-13 10:37:36 +0100

    New button for upcoming movies page.

--- src/pages/movieUpcomingPage.js
+++ src/pages/movieUpcomingPage.js
@@ -3,7 +3,7 @@ import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
 import { getUpcoming } from '../api/tmdb-api'
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import AddToFavouritesIcon from '../components/cardIcons/AddToPlaylist'
 
 const UpcomingPage = (props) => {
   const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcoming)
