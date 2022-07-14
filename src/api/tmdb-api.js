136426060f7c6e8d7dc4e617686a8b01fb6efd87
Anders <77015471+ingelsten@users.noreply.github.com>
2022-07-14 13:25:19 +0100

    Change upcoming movies page to use caching.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -28,6 +28,20 @@ export const getMovie = (args) => {
     throw error
  });
 };
+
+export const getUpcoming = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming/?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error
+    });
+  };
   
   export const getGenres = async () => {
     return fetch(
@@ -73,17 +87,5 @@ export const getMovie = (args) => {
       });
   };
 
-  export const getUpcoming = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-      ).then((response) => {
-        if (!response.ok) {
-          throw new Error(response.json().message);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-         throw error
-      });
-    };
+
       
