e59db91dbd259bc6f329bac2c6dae76249b39ab1
Anders <77015471+ingelsten@users.noreply.github.com>
2022-08-03 21:00:33 +0100

    Attempt to refactor the TV series and Person list pages

---
+++ src/components/templatePersonListPage/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import Header from "../headerPersonList";
+import FilterCard from "../filterPersonsCard";
+import Grid from "@material-ui/core/Grid";
+import Fab from "@material-ui/core/Fab";
+import Drawer from "@material-ui/core/Drawer";
+import { makeStyles } from "@material-ui/core/styles";
+import PersonList from "../personList";
+
+const useStyles = makeStyles((theme) =>  ({
+  root: {
+    padding: "20px",
+  },
+  fab: {
+    marginTop: theme.spacing(8),
+    position: "fixed",
+    top: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+function PersonListPageTemplate({ persons, name, selectFavourite }) {
+  const classes = useStyles();
+  const [nameFilter, setNameFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const genreId = Number(genreFilter);
+
+  let displayedPersons = persons
+    .filter((m) => {
+      return m.name.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+    })
+    .filter((m) => {
+      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+    });
+
+  const handleChange = (type, value) => {
+    if (type === "name") setNameFilter(value);
+    else setGenreFilter(value);
+  };
+
+  return (
+    <>
+    <Grid container className={classes.root}>
+      <Grid item xs={12}>
+        <Header name={name} />
+      </Grid>
+      <Grid item container spacing={5}>
+        <PersonList selectFavourite={selectFavourite} persons={displayedPersons} />
+      </Grid>
+    </Grid>
+    <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() => setDrawerOpen(true)}
+        className={classes.fab}
+      >
+        Filter
+      </Fab>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <FilterCard
+          onUserInput={handleChange}
+          nameFilter={nameFilter}
+          genreFilter={genreFilter}
+        />
+      </Drawer>
+    </>    
+  );
+}
+export default PersonListPageTemplate;
